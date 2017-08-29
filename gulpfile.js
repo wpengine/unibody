49b6fb0736851f4e60dd59df9e28214b12f62ee1
Ryan Hoover <ryan.hoover@wpengine.com>
2017-08-29 16:52:48 -0500

    start at an npm package for new phoenix styles

--- gulpfile.js
+++ gulpfile.js
@@ -2,60 +2,41 @@
 var fractal = "node_modules/@frctl/fractal/bin/fractal";
 
 // Package variables
-var gulp      = require('gulp'); // core gulp
-var sass = require('gulp-sass'); // compile scss
-var ghPages = require('gulp-gh-pages'); // deploy to github pages
-var shell = require('gulp-shell'); // allows shell commands
-var git = require('gulp-git');
-var svg2png = require('gulp-svg2png');
+var gulp = require( 'gulp' ); // core gulp
+var sass = require( 'gulp-sass' ); // compile scss
+var ghPages = require( 'gulp-gh-pages' ); // deploy to github pages
+var shell = require( 'gulp-shell' ); // allows shell commands
 
 //
 // Tasks
 //
 
 // Compile scss
-gulp.task('sass', function () {
-	return gulp.src('./src/scss/wpe/style.scss')
-		.pipe(sass().on('error', sass.logError))
-		.pipe(gulp.dest('./public/css/'));
-});
+gulp.task( 'sass', function () {
+    return gulp.src( './assets/stylesheets/wpengine.scss' )
+        .pipe( sass().on( 'error', sass.logError ) )
+        .pipe( gulp.dest( './public/css/' ) );
+} );
 
 // Run fractal's built-in build command
-gulp.task('build', shell.task([
-	fractal + ' build'
-]));
+gulp.task( 'build', shell.task( [
+    fractal + ' build'
+] ) );
 
 // Deploy site to GitHub pages
-gulp.task('gh-pages', function() {
-	return gulp.src('./build/**/*')
-		.pipe(shell([
-			]))
-		.pipe(ghPages());
-});
+gulp.task( 'gh-pages', function () {
+    return gulp.src( './build/**/*' )
+        .pipe( shell( [] ) )
+        .pipe( ghPages() );
+} );
 
 // Run fractal server
-gulp.task('watch', shell.task([
-	fractal + ' start --sync --watch'
-]));
-
-// Clone remote repo to sub folder ($CWD/sub/folder/git-test)
-gulp.task('get-svg-icons', function() {
-	git.clone('git@github.com:wpengine/wpengine-svg-icons.git', {args: './vendor/wpengine-svg-icons'}, function(err) {
-		// handle err
-	});
-	gulp.src('./vendor/wpengine-svg-icons/dist/svg/*.svg')
-			.pipe(gulp.dest('./public/img/svg-icons'));
-});
-
-gulp.task('svg2png', function () {
-	gulp.src('./vendor/wpengine-svg-icons/dist/svg/**/*.svg')
-		.pipe(svg2png())
-		.pipe(gulp.dest('./public/img/png-icons'));
-});
+gulp.task( 'watch', shell.task( [
+    fractal + ' start --sync --watch'
+] ) );
 
 //
 // Gulp sequential tasks
 //
-gulp.task('svg', ['get-svg-icons', 'svg2png']);
-gulp.task('serve', ['sass', 'watch']);
-gulp.task('deploy', ['sass', 'build', 'gh-pages']);
+gulp.task( 'serve', [ 'sass', 'watch' ] );
+gulp.task( 'deploy', [ 'sass', 'build', 'gh-pages' ] );
